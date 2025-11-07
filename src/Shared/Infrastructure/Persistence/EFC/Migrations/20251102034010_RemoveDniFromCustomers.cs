62832b43b29a61993f9b48109d20528fd6cf101a
Radv2005 <U202312318@upc.edu.pe>
2025-11-06 23:02:06 -0500

    feat: unversioned files

---
+++ src/Shared/Infrastructure/Persistence/EFC/Migrations/20251102034010_RemoveDniFromCustomers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlguienDijoChamba.Api.src.Shared.Infrastructure.Persistence.EFC.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveDniFromCustomers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dni",
+                table: "Customers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Dni",
+                table: "Customers",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
---
+++ src/Shared/Infrastructure/Persistence/EFC/Migrations/20251106015712_AddProfessionalTagCollectionToProfessional.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlguienDijoChamba.Api.src.Shared.Infrastructure.Persistence.EFC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfessionalTagCollectionToProfessional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProfessionalTags_Tags_TagId1",
+                table: "ProfessionalTags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProfessionalTags_TagId1",
+                table: "ProfessionalTags");
+
+            migrationBuilder.DropColumn(
+                name: "TagId1",
+                table: "ProfessionalTags");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProfessionalTags_Professionals_ProfessionalId",
+                table: "ProfessionalTags",
+                column: "ProfessionalId",
+                principalTable: "Professionals",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProfessionalTags_Professionals_ProfessionalId",
+                table: "ProfessionalTags");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TagId1",
+                table: "ProfessionalTags",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProfessionalTags_TagId1",
+                table: "ProfessionalTags",
+                column: "TagId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProfessionalTags_Tags_TagId1",
+                table: "ProfessionalTags",
+                column: "TagId1",
+                principalTable: "Tags",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
