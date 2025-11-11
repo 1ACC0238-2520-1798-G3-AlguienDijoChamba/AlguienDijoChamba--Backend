9a68895d516d6f7106d00150de5bd601d325b929
ghostnotfound404 <dbbastidas08@gmail.com>
2025-11-11 11:39:14 -0500

    fix: added backend

--- src/Professionals/Interfaces/Dtos/UpdateProfileRequest.cs
+++ src/Professionals/Interfaces/Dtos/UpdateProfileRequest.cs
@@ -1,5 +1,4 @@
-﻿namespace AlguienDijoChamba.Api.src.Professionals.Interfaces.Dtos;
-
+﻿
 // DTO para la actualización de datos del perfil (sin nombres/apellidos)
 public record UpdateProfileRequest(
     string Email,
